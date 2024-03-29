c87d097063348c6c3f425cc5340f55431f523c87
mariams73 <marmarmar73@alum.us.es>
2024-03-29 22:37:35 +0100

    feat: añadida funcionalidad de actualización de roles en la db y refactorización

--- cluedAI/main.py
+++ cluedAI/main.py
@@ -1,12 +1,13 @@
 import os
 from dotenv import load_dotenv
-from db.db_operations import flush_db, setup_db, randomize
+from db.db_operations import flush_db, setup_db, randomize, connect_db
 
 def main():
     load_dotenv()
     flush_db()
     api_key = os.getenv('OPENAI_API_KEY')
     setup_db()
+    randomize()
 
 if __name__ == "__main__":
     main()
