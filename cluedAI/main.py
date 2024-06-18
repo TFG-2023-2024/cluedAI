0e465c2e9e5f48ebf0f6595756dc93162fd1d98a
joaquin-restoy <73226465+joaquin-restoy@users.noreply.github.com>
2024-06-18 20:25:50 +0200

    feat: enlace a la db desplegada

--- cluedAI/main.py
+++ cluedAI/main.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 from db.db_operations import flush_db, setup_db, randomize, start_day
 from screen_manager import basic_window
 
-'''
+
 def start_mongodb():
     mongodb_path = os.path.join(os.path.dirname(__file__), 'mongodb/bin/mongod')
     dbpath = os.path.join(os.path.dirname(__file__), 'mongodb/data')
@@ -16,23 +16,23 @@ def start_mongodb():
     mongodb_process = subprocess.Popen([mongodb_path, '--dbpath', dbpath])
     time.sleep(5)  # Espera a que MongoDB inicie
     return mongodb_process
-'''
+
 
 def main():
     load_dotenv()
-    '''
+
     mongodb_process = start_mongodb()  # Inicia MongoDB
     try:
-    '''
-    flush_db()
-    setup_db()
-    basic_window()  # Llama a la función de inicio de la GUI
-    '''
+
+        flush_db()
+        setup_db()
+        basic_window()  # Llama a la función de inicio de la GUI
+
     finally:
         # Asegúrate de cerrar MongoDB cuando la aplicación termine
         mongodb_process.terminate()
         mongodb_process.wait()
-    '''
+
 
 if __name__ == "__main__":
     main()
